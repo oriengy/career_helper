f616c65738dda122b79e4b171bdd5309d6f7b0dd
1833429505@qq.com <1833429505@qq.com>
2026-01-11 21:42:34 +0800

    gai2

--- rebuild.sh
+++ rebuild.sh
@@ -9,11 +9,11 @@ cd "$ROOT_DIR"
 echo "[1/4] haha"
 
 echo "[2/4] Stop existing containers"
-docker compose -f "$COMPOSE_FILE" down --remove-orphans
+docker-compose -f "$COMPOSE_FILE" down --remove-orphans
 
 echo "[3/4] Build images"
-docker compose -f "$COMPOSE_FILE" build
+docker-compose -f "$COMPOSE_FILE" build
 
 echo "[4/4] Start services"
-docker compose -f "$COMPOSE_FILE" up -d
+docker-compose -f "$COMPOSE_FILE" up -d
 echo "[4/4] Done"
